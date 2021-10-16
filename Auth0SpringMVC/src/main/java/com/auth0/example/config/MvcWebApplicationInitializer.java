f435d34fd8f42ecc8d49067c29b607ed5264ec1e
akhil90s <46933461+akhil90s@users.noreply.github.com>
2021-10-16 17:26:48 +0530

    Securing a Spring MVC Web Application using Auth0

---
+++ Auth0SpringMVC/src/main/java/com/auth0/example/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.auth0.example.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { MvcWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
---
+++ Auth0SpringMVC/src/main/java/com/auth0/example/mvc/ErrorController.java
@@ -0,0 +1,30 @@
+package com.auth0.example.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+
+@Controller
+public class ErrorController  {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static final String PATH = "/error";
+
+    @RequestMapping("/error")
+    protected String error(final RedirectAttributes redirectAttributes) throws IOException {
+        logger.error("Handling error");
+        redirectAttributes.addFlashAttribute("error", true);
+        return "redirect:/login";
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
